bdc23ff74b645f7aa69097ee9508b150bde659e7
Lucas Oliveira <lucassdeoliveira@outlook.com.br>
2024-03-26 22:27:35 -0300

    Alterando a posição da foto, melhorando o botão e iniciando o media query

--- app.js
+++ app.js
@@ -1,25 +1,15 @@
 const nome = "Lucas de Oliveira";
-const vagaDesejada = "Sou um ambicioso desenvolvedor backend";
 const h1 = document.querySelector(".myNameText");
 const h2 = document.querySelector(".myJobText");
 
 function textoAnimado() {
     let index = 0;
-    let index2 = 0;
     const intervalo = setInterval(() => {
         if (index < nome.length) {
             h1.innerHTML += nome[index];
             index++;
         } else {
             clearInterval(intervalo);
-        }
-    }, 200)
-    const novoIntervalo = setInterval(() => {
-        if (segundoIndex < vagaDesejada.length) {
-            h2.innerHTML += vagaDesejada[index2];
-            index2++;
-        } else {
-            clearInterval(novoIntervalo)
         }
     }, 200)
 }
